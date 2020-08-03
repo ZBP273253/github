ccdc252f0ddd752fcf7c0516501ccdecc7c704ba
ZBP273253 <2732530938@qq.com>
2020-08-03 14:25:59 +0800

    Initial commit

---
+++ com/company/User.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class User {
+    private int id;
+    private String name;
+    private String addr;
+    private String sex;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
